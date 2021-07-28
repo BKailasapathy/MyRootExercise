d9c9cd621b8d77c50fbdc1bd3230955d589daa4f
Balashangar Kailasapathy <87571423+BKailasapathy@users.noreply.github.com>
2021-07-28 13:20:14 +0530

    Create graph.c

--- graph.c
+++ graph.c
@@ -2,7 +2,7 @@
   double x[8] = {1, 2, 3, 4, 5, 6, 7, 8};
   double y[8] = {1, 4, 7, 12, 8, 6, 8, 10};
   
-  TGraph gr{5, x, y};
-  TCanvus c1;
+  TGraph gr{8, x, y};
+  TCanvas c1;
   gr.Draw("AC*");
 }
