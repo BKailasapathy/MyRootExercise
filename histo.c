c758b930c66ac3eecf38628ff6b04daef2bf51e9
Balashangar Kailasapathy <87571423+BKailasapathy@users.noreply.github.com>
2021-07-28 12:46:26 +0530

    Update histo.c

--- histo.c
+++ histo.c
@@ -1,9 +1,9 @@
 {
   TH1F hist("hist","Histogram",100,-5,5);
   TRandom2 r;
-  for(int i; i<1000; i++)
+  for(int i = 0; i < 1000; i++)
   {
-    hist.Fill(r.gaus());
+    hist.Fill(r.Gaus());
   }
   
   TCanvas *c1 = new TCanvas();
