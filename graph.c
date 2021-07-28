8b9d90c6e7346a902c4ad4f95c4aa25951ff72da
Balashangar Kailasapathy <87571423+BKailasapathy@users.noreply.github.com>
2021-07-28 22:06:39 +0530

    Update graph.c

--- graph.c
+++ graph.c
@@ -2,7 +2,11 @@
   double x[8] = {1, 2, 3, 4, 5, 6, 7, 8};
   double y[8] = {1, 4, 7, 12, 8, 6, 8, 10};
   
-  TGraph gr{8, x, y};
+  // TGraph gr{8, x, y};
+  TGraph *gr = new TGraph(8,x,y);
+  
+  
   TCanvas c1;
-  gr.Draw("AC*");
+  //gr.Draw("AC*");
+  gr -> Draw("AC");
 }
